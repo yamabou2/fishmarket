8c38d7d7cf1462a8f6a0e5b7926e354883a74034
yamabouz <52478552+yamabouz@users.noreply.github.com>
2025-10-20 01:28:30 +0900

    新規作成

---
+++ src/main/java/jp/spring/artisan/FishmarketApplication.java
@@ -0,0 +1,13 @@
+package jp.spring.artisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishmarketApplication.class, args);
+	}
+
+}
